6759a2027490c57766f44967f0895d9939eb0cd1
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2024-07-10 02:26:51 -1200

    Delegate

---
+++ Abo Hadhod/Extension Methods.cs
@@ -0,0 +1,36 @@
+﻿// extension methods
+// الكلاس والميذد تكون استاتك نضيف للميذد this
+
+//int percen = 10;
+////if (IntExtension.IsBetween(percen , 0 , 100)) => instence method
+//if (percen.IsBetween(0, 100)) // => extension method
+
+//    Console.WriteLine("valid");
+//else
+//    Console.WriteLine("Invalid");
+
+
+//Console.WriteLine("Enter value:");
+//var input = Console.ReadLine();
+//Console.WriteLine(input.Remove().Reverse());
+
+
+
+
+//public static class IntExtension
+//{
+//    public static bool IsBetween(this int value, int min, int max)
+//    {
+//        return value >= min && value <= max;
+//    }
+//    public static string Remove(this string value)
+//    {
+//        return value.Replace(" ", "");
+//    }
+//    public static string Reverse(this string value)
+//    {
+//        var charArry = value.ToCharArray();
+//        Array.Reverse(charArry);
+//        return new string(charArry);
+//    }
+//}
--- Abo Hadhod/Program.cs
+++ Abo Hadhod/Program.cs
@@ -1,36 +1,101 @@
-﻿// extension methods
-// الكلاس والميذد تكون استاتك نضيف للميذد this
+﻿// Recursion ريكرجن مثال يعطي مضروب الرقم
+// f(5) = 5*4*3*2*1
+// static int CalculateFactorial(int number)
+//{
+//    if (number <= 1)
+//        return number;
 
-//int percen = 10;
-////if (IntExtension.IsBetween(percen , 0 , 100)) => instence method
-//if (percen.IsBetween(0, 100)) // => extension method
-  
-//    Console.WriteLine("valid");
-//else
-//    Console.WriteLine("Invalid");
+//    return number * CalculateFactorial(number - 1);
+//}
 
-
-Console.WriteLine("Enter value:");
-var input = Console.ReadLine();
-Console.WriteLine(input.Remove().Reverse());
+//Console.WriteLine(CalculateFactorial(5));
 
 
 
+// Delegates => فانكشن بوينتر نستخدمها لي يكون لنا رفرنس لميذد معينه اقدر اناديها في اي وقت من غير ماكون عارف مين هي ديه الميذد اصلا ولا موجوده فين
+// الي كل اعرفه عنها عدد البراميتر
 
-public static class IntExtension
+namespace AboHadhod;
+
+public class Program
 {
-    public static bool IsBetween(this int value, int min, int max)
+    delegate int CalculateDelegate(int num1, int num2); 
+    static void Main(string[] args)
     {
-        return value >= min && value <= max;
+        int num1 = 10;
+        int num2 = 2;
+        CalculateWithDelegate(num1, num2, Add);
+        CalculateWithDelegate(num1, num2, Sub);
+        CalculateWithDelegate(num1, num2, Multiply);
+        CalculateWithDelegate(num1, num2, Divide);
+        CalculateWithDelegate(num1, num2, delegate (int num1,int num2) { return num1 * num2; }); //  انونمس مالهش اسم
+        CalculateWithDelegate(num1, num2, (int num1, int num2) => num1 * num2); // هذا افضل سنتكس اسمها Lampda Expression لمدا اكسبريشن
+        CalculateWithDelegate(num1, num2, (x, y) => x * y); //ابسط اختصار
     }
-    public static string Remove(this string value)
+
+    static void CalculateWithDelegate(int num1, int num2, CalculateDelegate dle)
     {
-        return value.Replace(" ", "");
+        int result = dle(num1, num2);
+      
+        Console.WriteLine(result);
     }
-    public static string Reverse(this string value)
+
+    static int Add(int num1, int num2)
     {
-       var charArry = value.ToCharArray();
-        Array.Reverse(charArry);
-        return new string(charArry);
+        Console.WriteLine("Add");
+        return num1 + num2;
+
+    }
+    static int Sub(int num1, int num2)
+    {
+        Console.WriteLine("Sub");
+        return num1 - num2;
+
+    }
+    static int Multiply(int num1, int num2)
+    {
+        Console.WriteLine("Multiply");
+        return num1 * num2;
+
+    }
+    static int Divide(int num1, int num2)
+    {
+        Console.WriteLine("Divide");
+        return num1 / num2;
+
     }
 }
+
+
+
+
+//public class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        int num1 = 10;
+//        int num2 = 2;
+//        Calculate(num1, num2, '+');
+//        Calculate(num1, num2, '-');
+//        Calculate(num1, num2, '*');
+//        Calculate(num1, num2, '/');
+
+//    }
+
+//    static void Calculate(int num1, int num2, char o)
+//    {
+//        int result = 0;
+//        if (o == '+')
+//            result = num1 + num2;
+//        else if (o == '-')
+//            result = num1 - num2;
+//        else if (o == '*')
+//            result = num1 * num2;
+//        else if (o == '/')
+//            result = num1 / num2;
+//        else
+//            throw new ArgumentException("Invalid");
+//        Console.WriteLine(result);
+//    }
+
+//}
