e505416f354f029328511c0b33b67c876dbc9e4a
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2024-07-13 01:03:21 -1200

    LINQ Done

---
+++ Abo Hadhod/LINQ.cs
@@ -0,0 +1,83 @@
+﻿
+////LINQ//LINQ فايدتها تنفيذ مؤجل
+//// الكويري تتنفذ لما تعمل عليها لوب
+//using static System.Runtime.InteropServices.JavaScript.JSType;
+
+//public class LINQ
+//{
+//    /* 
+//     * projection and Filtering (select , where , Oftype )
+//     * Sorting (OrderBy, OrderByDescending, ThenBy, ThenByDescending, Reverse)
+//     * Quantifiers (All,Any, Contains)
+//     * Partitioning (Skip, SkipWhile, Take, TakeWhile, Chunk)
+//     * Set Operations (Distinct, DistinctBy, Except. exeptBy, Intersect, IntersectBy, Union, UnionBy)
+//     * Joins (Join, GroupJoen)
+//     * Grouping (GroupBy, ToLookup)
+//     */
+
+
+
+//    static async Task Main(string[] args)
+//    {
+
+
+
+
+//        var numbers = new int[] { 1, 10, 2, 3, 4, 5, 6, 7, 8, 9 };
+//        //Sorting
+//        //  var numbers = new List<int> { 10, 3, 2,  4, 7, 5, 9, 6,  8, 11, 1, 12 };
+//        //رتب تنازلي
+//        var result = from number in numbers where number > 5 orderby number descending select number;
+//        // رتب عادي
+//        result = numbers.Where(x => x > 4).OrderBy(x => x);
+//        // ميديت اكسكيوشن
+//        //var result = (from number in numbers where number > 5 select number).ToList();
+//        foreach (var number in result)
+//        {
+//            Console.WriteLine(number);
+//        }
+
+
+
+//        //var numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+//        ////اسم الطريقه كويري اكسبريسن 
+//        //var result = from number in numbers where number > 5 select number;
+//        //// اسمها method syntax
+//        //result = numbers.Where(x => x > 8);
+//        //// ميديت اكسكيوشن
+//        ////var result = (from number in numbers where number > 5 select number).ToList();
+//        //foreach (var number in result)
+//        //{
+//        //    Console.WriteLine(number);
+//        //}
+//        //Console.WriteLine("-------------------------");
+//        //numbers.AddRange(new int[] { 13, 14, 15 }); // or with list   numbers.AddRange(new List<int> { 13, 14, 15 });
+//        //foreach (var number in result)
+//        //{
+//        //    Console.WriteLine(number);
+//        //}
+
+
+//        //int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+//        //var result = from number in numbers where number > 5 select number;
+//        //foreach (var number in result)
+//        //{
+//        //    Console.WriteLine(number);
+//        //}
+//        // نفس العمل باستخدام LINQ
+//        //var result = new List<int>();
+//        //foreach (var number in numbers)
+//        //{
+//        //    if(number > 5)
+//        //    {
+//        //        result.Add(number);
+//        //        Console.WriteLine(number);
+//        //    }
+//        //}
+
+//    }
+
+
+//}
+
+
--- Abo Hadhod/Program.cs
+++ Abo Hadhod/Program.cs
@@ -1,66 +1,76 @@
 ﻿
 //LINQ//LINQ فايدتها تنفيذ مؤجل
 // الكويري تتنفذ لما تعمل عليها لوب
+
+using System.ComponentModel;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
-public class Program 
+public class Program
 {
     /* 
-     * projection and Filtering (select , where , Oftype )
-     * Sorting (OrderBy, OrderByDescending, ThenBy, ThenByDescending, Reverse)
+ 
      * Quantifiers (All,Any, Contains)
      * Partitioning (Skip, SkipWhile, Take, TakeWhile, Chunk)
+     * مثل البحث في قوقل يجيب عدة صفحات
      * Set Operations (Distinct, DistinctBy, Except. exeptBy, Intersect, IntersectBy, Union, UnionBy)
      * Joins (Join, GroupJoen)
+     * يربط لك بين ادارتين بال ا دي 
      * Grouping (GroupBy, ToLookup)
      */
-
-
+    
+    
+    
     static async Task Main(string[] args)
     {
-        var numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
-        //اسم الطريقه كويري اكسبريسن 
-        var result = from number in numbers where number >5 select number;
-        // اسمها method syntax
-        result = numbers.Where(x => x > 8);
-        // ميديت اكسكيوشن
-        //var result = (from number in numbers where number > 5 select number).ToList();
-        foreach ( var number in result)
+        //Set Operations
+        //Distinct تحذف المكررات
+        //Intersect تقاطع
+        //Union اتحاد
+        var n = new List<int> { 1,1,6,2,5, 5, 6, 2, 9, 7, 4, 8, 10, 11, 12, };
+        //var query = n.Distinct().OrderBy(x => x);
+        var secondList = new List<int> { 1, 2, 4, 5,45,65,88 };
+        // يحذف من اللست اللست الجدبده
+        //var query = n.Distinct().Except(secondList).OrderBy(x => x);
+        var query = n.Union(secondList).OrderBy(x => x);
+
+        foreach (var x in query)
         {
-            Console.WriteLine( number );
+            Console.WriteLine(x);
         }
-        Console.WriteLine("-------------------------");
-        numbers.AddRange(new int[] { 13, 14, 15 }); // or with list   numbers.AddRange(new List<int> { 13, 14, 15 });
-        foreach ( var number in result)
+
+
+
+
+
+        // Quantifiers(All, Any, Contains)
+        var number = new List<int> { 1, 5, 6, 2, 9, 7, 4, 8, 10, 11, 12, };
+        //var result = number.All(x => x > 0);
+        //var result = number.Any(x => x < 0);
+        var result = number.Contains(10);
+        Console.WriteLine(result);
+
+
+
+        //Partitioning (Skip, SkipWhile, Take, TakeWhile, Chunk)
+        var num = new int[] { 1,2,3,4, 5, 6,7,8,9,10 };
+        // var result2 = num.Skip(4).Take(6);
+        //var result2 = num.SkipWhile(x => x  < 7);
+        //تقسم اللست الى مجموعات من رقمين
+        var result2 = num.Chunk(2);
+        foreach (var C in result2)
         {
-            Console.WriteLine(number);
+           foreach(var C2 in C)
+            {
+
+
+                Console.WriteLine(C2);
+            }
+           Console.WriteLine("-----------");
         }
-      
-
-
-
-        //int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
-        //var result = from number in numbers where number > 5 select number;
-        //foreach (var number in result)
-        //{
-        //    Console.WriteLine(number);
-        //}
-        // نفس العمل باستخدام LINQ
-        //var result = new List<int>();
-        //foreach (var number in numbers)
-        //{
-        //    if(number > 5)
-        //    {
-        //        result.Add(number);
-        //        Console.WriteLine(number);
-        //    }
-        //}
 
     }
 
- 
+
 }
-
-
 
 
